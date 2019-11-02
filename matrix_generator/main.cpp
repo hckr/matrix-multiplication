1cf14c6aad6529312870eb1f044b1169816bad39
Luke <luke@luke.co.pl>
2019-11-02 01:57:48 +0100

    Add more usage info for matrix generator, small refactor

--- matrix_generator/main.cpp
+++ matrix_generator/main.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <random>
+#include <sstream>
 
-size_t integer_part_size(std::string str) {
+std::size_t integer_part_size(std::string str) {
     return str.substr(0, str.find('.')).size();
 }
 
-size_t fractional_part_size(std::string str) {
+std::size_t fractional_part_size(std::string str) {
     auto decimal_point_pos = str.find('.');
     if (decimal_point_pos == std::string::npos) {
         return 0;
@@ -16,19 +17,40 @@ size_t fractional_part_size(std::string str) {
 int main(int argc, char const *argv[]) {
     if (argc != 5) {
         printf("Usage: %s num_rows num_cols min_val max_val\n", argv[0]);
+        puts("\nIn the first line of output there are two numbers separated by a space: "
+             "number of rows and columns of the generated matrix. Next lines are consecutive "
+             "rows of the matrix consisting of random numbers between min_val and max_val "
+             "separated by spaces. Length of the fractional part is determined based on "
+             "the input values of min_val and max_val.\n\nExamples:\n");
+        printf("%s 3 3 -1 1\n", argv[0]);
+        puts("Could output for example:\n"
+             "3 3\n"
+             " 0  0  1 \n"
+             "-1 -1 -1 \n"
+             " 0 -1 -0 \n");
+        printf("%s 3 4 -1.00 1.00\n", argv[0]);
+        puts("Could output for example:\n"
+             "3 4\n"
+             "-0.45 -0.73 -0.74 -0.32 \n"
+             " 0.10 -0.73  0.58 -0.10 \n"
+             "-0.23  0.30 -0.22  0.09 ");
         return 1;
     }
 
-    const int num_rows = std::atoi(argv[1]);
-    const int num_cols = std::atoi(argv[2]);
+    unsigned num_rows;
+    std::istringstream(argv[1]) >> num_rows;
+    unsigned num_cols;
+    std::istringstream(argv[2]) >> num_cols;
 
     if (num_rows <= 0 || num_cols <= 0) {
         puts("Error: please provide posivie matrix dimensions.\n");
         return 2;
     }
 
-    const double min_val = std::atof(argv[3]);
-    const double max_val = std::atof(argv[4]);
+    double min_val;
+    std::istringstream(argv[3]) >> min_val;
+    double max_val;
+    std::istringstream(argv[4]) >> max_val;
 
     const int int_len = std::max(integer_part_size(argv[3]), integer_part_size(argv[4]));
     const int frac_len = std::max(fractional_part_size(argv[3]), fractional_part_size(argv[4]));
@@ -43,8 +65,8 @@ int main(int argc, char const *argv[]) {
 
     printf("%d %d\n", num_rows, num_cols);
 
-    for (int r = 0; r < num_rows; ++r) {
-        for (int c = 0; c < num_cols; ++c) {
+    for (unsigned r = 0; r < num_rows; ++r) {
+        for (unsigned c = 0; c < num_cols; ++c) {
             printf("%*.*f ", whole_len, frac_len, dis(gen));
         }
         puts("");
